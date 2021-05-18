8cf67f8bece5a63bdbfdf19a9a3ec49cf23929f3
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-05-18 15:51:49 +0700

    commit

---
+++ api/src/config.ts
@@ -0,0 +1,17 @@
+import merge from 'lodash.merge'
+import fs from 'fs'
+
+const config = {
+    name : 'test',
+    api : {
+        port :80,
+    },
+    database : {
+        username : 'testmongoDB',
+        password : '15918Petch',
+        host : 'testmongoDB',
+        port : 27017,
+        database : 'test'
+    }
+
+}
